2daa8b2a86b13159fad3110f6c6689f601e80d2a
Luiz Francisco <luizfrancisco2000@gmail.com>
2018-08-01 16:59:00 -0400

    teste

--- app/src/main/java/com/example/luiz/teacherassistent/Interface/CadastroQuestao/CadastrarResolucao.java
+++ app/src/main/java/com/example/luiz/teacherassistent/Interface/CadastroQuestao/CadastrarResolucao.java
@@ -1,11 +1,16 @@
 package com.example.luiz.teacherassistent.Interface.CadastroQuestao;
 
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+
+import com.example.luiz.teacherassistent.Helper.ProcessSingleImageTask;
+
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -22,6 +27,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.luiz.teacherassistent.Controle.Questao;
+import com.example.luiz.teacherassistent.Helper.Base64Custom;
 import com.example.luiz.teacherassistent.Interface.Menus.MenuProfessor;
 import com.example.luiz.teacherassistent.R;
 import com.example.luiz.teacherassistent.Servidor.ConfiguracaoDataBase;
@@ -30,9 +36,23 @@ import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 import com.google.firebase.database.DatabaseReference;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
 
+import junit.framework.Assert;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 
 public class CadastrarResolucao extends AppCompatActivity {
@@ -43,11 +63,14 @@ public class CadastrarResolucao extends AppCompatActivity {
     protected final int GALERIA_IMAGENS = 1;
     private FloatingActionButton concluirCadastro;
     //constantes e variaveis
-    private final int  PERMISSAO_REQUEST =2;
+    private final int PERMISSAO_REQUEST = 2;
     private Questao questao;
     // ferramentas
     private TextRecognizer ocrResolucao;
     private Bitmap imageGaleria;
+    File filesDir;
+    File imageFile;
+    File testeBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,47 +85,82 @@ public class CadastrarResolucao extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent,GALERIA_IMAGENS);
+                startActivityForResult(intent, GALERIA_IMAGENS);
             }
         });
         concluirCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 questao.convertStringForArray(editResolucao.getText().toString());
-                if(questao.getResolucao().size()==1) {
+                if (questao.getResolucao().size() == 1) {
                     questao.salvar();
                     Intent intent = new Intent(CadastrarResolucao.this, MenuProfessor.class);
                     Toast.makeText(CadastrarResolucao.this, "Casdastro realizado com sucesso\n retomando ao menu", Toast.LENGTH_SHORT).show();
                     startActivity(intent);
-                }else{
+                } else {
                     atualizarBanco();
                 }
 
             }
         });
     }
-    public void validarPermissao(){
-        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)){
-            }else{
-                ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},PERMISSAO_REQUEST);
+
+    public void validarPermissao() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSAO_REQUEST);
             }
         }
 
     }
-    protected void  onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode==RESULT_OK && requestCode==GALERIA_IMAGENS){
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGENS) {
+            OkHttpClient client = new OkHttpClient();
             Uri selectedImage = data.getData();
             imagemResolucao.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             String[] filePath = {MediaStore.Images.Media.DATA};
-            Cursor c = getContentResolver().query(selectedImage,filePath,null,null,null);
+            Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
             c.moveToFirst();
             String picturePath = c.getString(c.getColumnIndex(filePath[0]));
             c.close();
             imagemResolucao.setDrawingCacheEnabled(true);
             imagemResolucao.buildDrawingCache();
             imageGaleria = (BitmapFactory.decodeFile(picturePath));
+            File f = persistImage(imageGaleria,"resolucao");
+            if(f.exists()) {
+                try {
+                    String result = new ProcessSingleImageTask().execute(testeBase).get();
+                    editResolucao.setText(result);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                Toast.makeText(this, "Coco", Toast.LENGTH_SHORT).show();
+            }
+            /*imageGaleria = (BitmapFactory.decodeFile(picturePath));
+            MediaType mediaType = MediaType.parse("application/json");
+
+            //RequestBody body = RequestBody.create(mediaType, Base64Custom.codificarBase64(filePath[0]));
+            RequestBody body = RequestBody.create(mediaType, "{ \"src\" : \"data:image/jpeg;base64,{BASE64-STRING}\" }");
+            Request request = new Request.Builder()
+                    .url("https://api.mathpix.com/v3/latex")
+                    .addHeader("content-type", "application/json")
+                    .addHeader("app_id", "mathpix")
+                    .addHeader("app_key", "34f1a4cea0eaca8540c95908b4dc84ab")
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                //editResolucao.setText(response.body().string());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }catch (Exception e){
+                Log.d("aa",e.getMessage());
+            }
             ocrResolucao = new TextRecognizer.Builder(imagemResolucao.getContext()).build();
             if(!ocrResolucao.isOperational()){
                 Log.w("Cadastro","Detector dependecies are not yet avaiable");
@@ -119,20 +177,68 @@ public class CadastrarResolucao extends AppCompatActivity {
             }
             Bitmap bitmapReduzido = Bitmap.createScaledBitmap(imageGaleria, 100, 100, true);
             imagemResolucao.setImageBitmap(bitmapReduzido);
+            */
         }
     }
-    public void atualizarBanco(){
+
+    public void atualizarBanco() {
         DatabaseReference datbase = ConfiguracaoDataBase.getFirebase();
         Map<String, Object> questaoSalvar = questao.toMap();
         Map<String, Object> questaoAtualizacoes = new HashMap<>();
-        questaoAtualizacoes.put("/questao/"+questao.getMateria()+"/"+questao.getAssunto()+"/",questaoSalvar);
+        questaoAtualizacoes.put("/questao/" + questao.getMateria() + "/" + questao.getAssunto() + "/", questaoSalvar);
         datbase.updateChildren(questaoAtualizacoes).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void aVoid) {
-                Log.d("Atualicao","Atualização feita com sucesso");
+                Log.d("Atualicao", "Atualização feita com sucesso");
                 Intent intent = new Intent(CadastrarResolucao.this, MenuProfessor.class);
                 startActivity(intent);
             }
         });
+    }
+    private File persistImage(Bitmap bitmap, String name) {
+
+        filesDir = getApplicationContext().getFilesDir();
+        imageFile = new File(filesDir, name + ".jpeg");
+         testeBase = new File("/data/data/" + getPackageName() + "/" + imageFile.getName());
+        OutputStream os;
+        try {
+            os = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
+            os.flush();
+            os.close();
+            return imageFile;
+        } catch (Exception e) {
+            Log.e(getClass().getSimpleName(), "Error writing bitmap", e);
+            return null;
+        }
+    }
+    private File getTestFile(File file) {
+        AssetManager assetManager = getAssets();
+        InputStream in;
+        OutputStream out;
+
+        try {
+            in = assetManager.open(file.getName());
+            File cloneFile = new File("/data/data/" + getPackageName() + "/" + file.getName());
+
+            if (cloneFile.exists()) return cloneFile;
+
+            out = new FileOutputStream(cloneFile);
+
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+            in.close();
+            out.flush();
+            out.close();
+
+            return cloneFile;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
